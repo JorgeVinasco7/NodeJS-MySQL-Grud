e3351a2ad12ebdb26c3470a12716e0f6a5ae80a3
JorgeVinasco7 <jorgevinasco_77@live.com>
2023-03-29 14:49:07 -0500

    Web-1.3

--- src/index.js
+++ src/index.js
@@ -3,33 +3,40 @@ const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const path = require('path');
 
-//inicializacion
-const app=express();
+//inicializar
+const app= express();
 
-//settings aqui colocamos las configuraciones
-app.set('port',process.env.PORT||4000);
-app.set('views',path.join(__dirname,'views'))
-app.engine('.hbs',exphbs({
-    defaultLayout: 'main',
+//settings
+app.set('port',process.env.PORT || 4000);
+app.set('views',path.join(__dirname,'views'));
+
+app.engine('.hbs',exphbs.engine({
+    defaultLayout:'main',
     layoutsDir: path.join(app.get('views'),'layouts'),
-    patialsDir: path.join(app.get('views'),'partials'),
+    partialsDir:path.join(app.get('views'),'partials'),
     extname: '.hbs',
     helpers: require('./lib/handlebars')
 }));
-app.set('view engine', '.hbs')
+app.set('view engine','.hbs');
 
-//Middleware
+//Middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
 
-//variables globales
+//Global Variables
+app.use((req,res,next)=> {
+    next();
+});
 
 //Routes
 app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/links',require('./routes/links'));
 
-//public 
+//public
 
-//inicializar el servidor
+//starting the server
 app.listen(app.get('port'),()=> {
-    console.log('Server on port',app.get('port'));
+   console.log('Server on port', app.get('port'));
 });
-
