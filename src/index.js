28051afc13c3a9f87d45d12cd2eeb4178eaec393
JorgeVinasco7 <jorgevinasco_77@live.com>
2023-03-29 17:59:21 -0500

    Web-1.4

--- src/index.js
+++ src/index.js
@@ -35,6 +35,7 @@ app.use(require('./routes/authentication'));
 app.use('/links',require('./routes/links'));
 
 //public
+app.use(express.static(path.join(__dirname,'public')));
 
 //starting the server
 app.listen(app.get('port'),()=> {
