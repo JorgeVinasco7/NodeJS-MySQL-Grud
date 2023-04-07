fdf00db82ac0def6e6dec4ead65743759467da0f
JorgeVinasco7 <jorgevinasco_77@live.com>
2023-04-07 11:39:12 -0500

    Web-1.6

--- src/routes/links.js
+++ src/routes/links.js
@@ -15,9 +15,25 @@ router.post('/add', async(req,res)=>{
           description
     };
     await pool.query('INSERT INTO links set ?', [newLink]);
-    res.send('received');
+    res.redirect('/links');
 });
 
+router.get('/', async(req,res)=> {
+     const links = await pool.query('SELECT * FROM links');
+     res.render('links/list',{ links });
+});
 
+router.get('/delete/:id', async (req,res)=>{
+     const { id } = req.params;
+     await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+     res.redirect('/links');
+});
+
+router.get('/edit/:id', async (req,res) => {
+     const { id } = req.params;
+     const links = await pool.query('SELECT *FROM links WHERE id = ?',[id]);
+     console.log(links);
+     res.render('links/edit',{links:links});
+});
 
 module.exports=router;
